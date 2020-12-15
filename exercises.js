8a4eb872db11e3970fb6ef0f5edbac175c157c0f
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2020-12-16 00:47:49 +0100

    add the variables to task 4

--- exercises.js
+++ exercises.js
@@ -3,38 +3,89 @@ function sumElements() {}
 
 function sumNumbers() {}
 
+
+
 // Zad 2
 function getInitials() {}
+
+
 // Zad 3
 function sumPositives() {}
+
+
 
 //BONUS
 function countPositivesAndSumNegatives() {
   // returns [positivesCount, negativeSum]
 }
 
+
+
 // Zad 4
-function praiseHardworkersBashLazyStudents() {}
+var students = [
+  {name: 'Janusz', tasksDone: 150, hoursSpent: 160},
+  {name: 'Karyna', tasksDone: 1, hoursSpent: 1},
+  {name: 'Krystek', tasksDone: 100, hoursSpent: 80},
+  {name: 'Seba', tasksDone: 200, hoursSpent: 40},
+  {name: 'Dagmara', tasksDone: 50, hoursSpent: 100}
+]
+
+ var requirements = {
+  minHoursSpent: 80,
+  minTasksDone: 100
+}
+
+
 // Zad 5
 function sumArrays() {}
+
+
+
 // Zad 6
 function canYouPlayBanjo() {}
+
+
+
 // Zad 7
 function reverseString() {}
+
+
+
 // Zad 8
 function enough() {}
+
+
+
 // Zad 9
 function multiplyTable() {}
+
+
+
 // Zad 10
 function canDivideBy() {}
+
+
+
 // Zad 11
 function ageDifferences() {}
+
+
+
 // Zad 12
 function gimmeMiddleIndex() {}
+
+
+
 // Zad 13
 function whatsMySuperVillainName() {}
+
+
+
 // Zad 14
 function checkExam() {}
+
+
+
 // Zad 15
 function getNiceNames(){}
 
