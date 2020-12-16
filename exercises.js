edc0f6c0d5030bcb9607034f858749244bdcc6d2
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2020-12-16 15:31:48 +0100

    task 1: add variables that store the values for three arrays

--- exercises.js
+++ exercises.js
@@ -1,7 +1,15 @@
 // Zad 1
-function sumElements() {}
+const arrA = [9, 3, '7', '3'];
+const arrB = ['5', '0', 9, 3, 2, 1, '9', 6, 7];
+const arrC = ['3', 6, 6, 0, '5', 8, 5, '6', 2,'0'];
 
-function sumNumbers() {}
+function sumElements() {
+ 
+}
+
+function sumNumbers() {
+
+}
 
 
 
